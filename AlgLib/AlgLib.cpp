fb72a56628def87c3e2358fd8cbf62c1cd2c1f18
Vitalii Demediuk <demedyuk.v.i@gmail.com>
2021-12-02 23:10:38 +0200

    Baby step, giagant step.

--- AlgLib/AlgLib.cpp
+++ AlgLib/AlgLib.cpp
@@ -1,10 +1,34 @@
 /// My header
 #include "AlgLib.h"
 
+/// STL
+#include <map>
+
 using namespace alglib;
 
 al::BigInt alglib::babyStepGiantStepLog(const al::BigInt& a,
                 const al::BigInt& b, const al::BigInt& p) noexcept
 {
-    return 0;
+    const al::BigInt n = al::sqrt(p) + 1;
+    const al::BigInt an = al::powMod(a, n, p);
+
+    std::map<al::BigInt, al::BigInt> np;
+    for (al::BigInt i = 1, tmp = an; i <= n; ++i) {
+        if (!np.count(tmp)) {
+            np[tmp] = i;
+        }
+        tmp = al::multiplyMod(tmp, an, p);
+    }
+
+    for (al::BigInt i = 0, tmp = b; i <= n; ++i) {
+        if (np.count(tmp)) {
+            al::BigInt ans = np[tmp] * n - i;
+            if (ans < p) {
+                return ans;
+            }
+        }
+        tmp = al::multiplyMod(tmp, a, p);
+    }
+
+    return -1;
 }
